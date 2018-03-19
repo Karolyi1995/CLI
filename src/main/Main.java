9897592501a7c0fe4615a788be9003429f7ddef9
Student <Student@EPHUDEBW0804.budapest.epam.com>
2018-03-19 09:49:31 +0100

    Megoldas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 import reflectionhandler.ReflectionHandler;
 
-/**
- *
- * @author Iskola02
- */
 public class Main {
     
     public static void main(String[] args) {
@@ -26,41 +21,118 @@ public class Main {
             
             switch(Integer.parseInt(sc.nextLine())){
                 case 1:listing();break;
-                case 2:
-                case 3:
-                case 4:
-                case 5:    
+                case 2:describe();break;
+                case 3:details();break;
+                case 4:invokeMethod();break;
+                case 5:System.exit(0);
             }
        }while(true);
     }
     
-    @SuppressWarnings("null")
     private static void listing(){
+        ReflectionHandler.visibilityMode(inputClass(),inputVisibility());
+    }
+    
+    private static void describe(){
+        ReflectionHandler.describe(inputClass(), inputAccessibleObject(), inputVisibility());
+    }
+    
+    private static void details(){
+        Class c = inputClass();
         Scanner sc = new Scanner(System.in);
+        System.out.println("Method or Field details?");
+        String input = sc.nextLine();
+        if("Field".equals(input)){
+            System.out.println("Fieldname: ");
+            String name = sc.nextLine();
+            ReflectionHandler.details(c,Field.class,name);
+        }else if("Method".equals(input)){
+            System.out.println("Methodname: ");
+            String name = sc.nextLine();
+            ReflectionHandler.details(c,Method.class,name);
+        }else{
+            System.err.println("Invalid input");
+        }
+    }
+    
+    private static void invokeMethod(){
+        Scanner sc = new Scanner(System.in);
+        Object o = null;
+        Class c = inputClass();
+        
+        System.out.println("Write the constructor parameters: ");
+        Class[] constparams = paramsForName(sc.nextLine().split(" "));
+        
+        System.out.println("Object to be created: ");
+        String[] newinstance = sc.nextLine().split(" ");
+        try {
+            Constructor con = c.getConstructor(constparams);
+            o = con.newInstance(newinstance);
+            
+        } catch (NoSuchMethodException ex) {
+            System.err.println("No such constructor is found");
+        } catch (ReflectiveOperationException ex){
+            System.err.println("Failed to invoke constructor");
+        }
+        
+        System.out.println("Write the method name to invoke: ");
+        String methodname = sc.nextLine();
+        System.out.println("Write the method parameters: ");
+        Class[] methodparams = paramsForName(sc.nextLine().split(" "));
+        
+        try {
+            ReflectionHandler.invokeMethod(o, Method.class.getMethod(methodname, methodparams));
+        } catch (NoSuchMethodException ex) {
+            System.err.println("No such method is found");
+        } 
+    }
+    
+    private static Class inputClass(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the class");
         Class classname = null;
-        
-        
-        System.out.println("Write the class.");
         try {
             classname = Class.forName(sc.nextLine());
         } catch (ClassNotFoundException ex) {
             System.err.println("Class not found");
         }
-        ReflectionHandler.visibilityMode(classname,visibility(),classname.getFields());
+        return classname;
     }
     
-    private static void describe(){
-        
-    }
-    
-    private static String visibility(){
+    private static String inputVisibility(){
         System.out.println("Visibility: non-private/all (Default: non-private will be given if visibility is invalid)");
-        Scanner sc = new Scanner(System.in) ;
+        Scanner sc = new Scanner(System.in);
         if("all".equals(sc.nextLine())){
             return "all";
-        }else{
-            return "non-private";
         }
+        return "non-private";
     }
+    
+    private static Class inputAccessibleObject(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Describe: Field/Constructor/Method  Default: Everything");
+        String s = sc.nextLine();
+        if("Field".equals(s)){
+            return Field.class;
+        }
+        if("Constructor".equals(s)){
+            return Constructor.class;
+        }
+        if("Method".equals(s)){
+            return Method.class;
+        }
+        return Object.class;
+    }
+     private static Class[] paramsForName(String[] params){
+         Class[] parameters = null;
+         try {
+            for(int i = 0;i<params.length;i++){
+                parameters[i] = Class.forName(params[i]);
+            } 
+         }catch (ClassNotFoundException ex) {
+             System.err.println("Class not found.");
+         }
+         return parameters;
+     }
     
 }
--- src/reflectionhandler/ReflectionHandler.java
+++ src/reflectionhandler/ReflectionHandler.java
@@ -1,70 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package reflectionhandler;
 
 import exceptions.InvalidInput;
-import commandlineinterface.CLI;
-import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-
-/**
- *
- * @author Student
- */
 public class ReflectionHandler {
     
-    public static void visibilityMode(Class c,String visibility,Field[] fields){
-        if("non-private".equals(visibility)){
-            for (Field field : fields) {
-                if ((field.getModifiers() & Modifier.PRIVATE) == 0) {
-                    System.out.println(field);
-                }
-            }
-        }else{
-            System.out.println(CLI.class.getFields());
+    public static void visibilityMode(Class c,String visibility){
+        printFields(c,visibility);
+        printConstructor(c,visibility);
+        printMethods(c,visibility); 
+    }
+    
+    public static void describe(Class c,Class o,String visibility){
+        if(!Field.class.equals(o.getClass()) && !Constructor.class.equals(o.getClass()) && !Method.class.equals(o.getClass())){
+            printFields(c,visibility);
+            printConstructor(c,visibility);
+            printMethods(c,visibility);
+        }
+        if(Field.class.equals(o.getClass())){
+            printFields(c,visibility);
+        }   
+        if(Constructor.class.equals(o.getClass())){
+            printConstructor(c,visibility);
+        }
+        if(Method.class.equals(o.getClass())){
+            printMethods(c,visibility);
         }
     }
     
-    public static void describe(Class c,AccessibleObject o,String visibility,String Objectname,Class... type){
-        try{
-            if(!Field.class.equals(o.getClass()) && !Constructor.class.equals(o.getClass()) && !Method.class.equals(o.getClass())){
-                System.out.println(c.getFields() + "\n" + c.getConstructors() + "\n" + c.getMethods());
-            }
-        
-            if(Field.class.equals(o.getClass())){
-                System.out.println(c.getField(Objectname));
-            }
-            if(Method.class.equals(o.getClass())){
-                System.out.println(c.getMethod(Objectname,type));
-            }
-        }catch(NoSuchFieldException e){
-            System.err.println("This field doesn't exist in the class");
-        }catch(NoSuchMethodException e){
-            System.err.println("This method doesn't exist");
-        }
-    }
-    
-    public static void details(Class c,AccessibleObject o,String FieldOrMethodname,Class... params) {
-        
+    public static void details(Class c,Class o,String FieldOrMethodname,Class... params) {
         try{
             if(!Field.class.equals(o.getClass()) && !Method.class.equals(o.getClass())){
                 throw new InvalidInput("Object isn't a type of field or method");
             }
-        
             if(Field.class.equals(o.getClass())){
                 Field f = c.getField(FieldOrMethodname);
-                System.out.println(f.getAnnotations() + " " + f.getType() + " " + decodeModifiers(f,f.getModifiers()));
+                System.out.println(f.getAnnotations() + " " + f.getType() + " " + decodeModifiers(f.getModifiers()));
             }
             if(Method.class.equals(o.getClass())){
                 Method m = c.getMethod(FieldOrMethodname, params);
-                System.out.println(m.getReturnType() + " " + m.getParameterTypes() + " " + m.getAnnotations());
+                System.out.println(m.getAnnotations() + " " + m.getReturnType() + " " + m.getParameterTypes() + " " + decodeModifiers(m.getModifiers()));
             }
         }catch(NoSuchFieldException e){
             System.err.println("This field doesn't exist in the class");
@@ -75,16 +55,79 @@ public class ReflectionHandler {
         }
     }
     
-    private static String decodeModifiers(AccessibleObject o,int mods){
+    public static void invokeMethod(Object o,Method methodname,Object... methodparams){
+        try{
+            methodname.invoke(o, methodparams);
+        } catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException ex){
+            System.err.println("Failed to invoke the method");
+        }
+    }
+    
+    private static String decodeModifiers(int mods){
         String Modifiers = "";
-        if ((Modifier.PUBLIC&mods)==1)      Modifiers += "public ";
-	if ((Modifier.PROTECTED&mods)==4)   Modifiers += "protected ";
-	if ((Modifier.PRIVATE&mods)==2)     Modifiers += "private ";
-	if ((Modifier.STATIC&mods)==8)      Modifiers += "static ";
-	if ((Modifier.FINAL&mods)==16)       Modifiers += "final ";
-	if ((Modifier.TRANSIENT&mods)==128)   Modifiers += "transient ";
-	if ((Modifier.VOLATILE&mods)==64)    Modifiers += "volatile ";
+        if ((Modifier.PUBLIC&mods)==1){
+            Modifiers += "public ";
+        }
+	if ((Modifier.PROTECTED&mods)==4){
+            Modifiers += "protected ";
+        }
+	if ((Modifier.PRIVATE&mods)==2){
+            Modifiers += "private ";
+        }
+	if ((Modifier.STATIC&mods)==8){
+            Modifiers += "static ";
+        }
+	if ((Modifier.FINAL&mods)==16){
+            Modifiers += "final ";
+        }
+	if ((Modifier.TRANSIENT&mods)==128){ 
+            Modifiers += "transient ";
+        }
+	if ((Modifier.VOLATILE&mods)==64){
+            Modifiers += "volatile ";
+        }
 	return Modifiers;
     }
     
+    public static void printFields(Class c,String visibility){
+        Field[] fields = c.getDeclaredFields();
+        if("non-private".equals(visibility)){
+            for(int i = 0;i<fields.length;i++){
+                if((fields[i].getModifiers() & Modifier.PRIVATE) == 0){
+                    System.out.println(decodeModifiers(fields[i].getModifiers()) + " " + fields[i].getType() + " " + fields[i].getName());
+                }
+            }
+        }else{
+            System.out.println(fields);
+        }
+    }
+    
+    public static void printConstructor(Class c,String visibility){
+        Constructor[] constructors = c.getDeclaredConstructors();
+        if("non-private".equals(visibility)){
+            for(int i = 0;i<constructors.length;i++){
+                Constructor co = constructors[i]; 
+                if((co.getModifiers() & Modifier.PRIVATE) == 0){
+                    System.out.println(decodeModifiers(co.getModifiers()) + " " + co.getName() + " " + co.getParameterTypes());
+                }
+            }
+        }else{
+            System.out.println(constructors);
+        }
+    }
+    
+    public static void printMethods(Class c,String visibility){
+        Method[] methods = c.getDeclaredMethods();
+        if("non-private".equals(visibility)){
+            for(int i = 0;i<methods.length;i++){
+                Method m = methods[i]; 
+                if((m.getModifiers() & Modifier.PRIVATE) == 0){
+                    System.out.println(decodeModifiers(m.getModifiers()) + " " + m.getReturnType() + " " + m.getName()
+                    + " " + m.getParameterTypes() + " " + m.getAnnotations());
+                }
+            }
+        }else{
+            System.out.println(methods);
+        }
+    }
 }
